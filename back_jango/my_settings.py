d005921aaf38d0e4fd96c456ad8e88952f742af3
htoh702 <htoh702@gmail.com>
2024-03-27 17:10:25 +0900

    first task

---
+++ back_jango/my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'docker_db',
+        'USER': 'test',
+        'PASSWORD': 'pass123#',
+        'HOST': 'localhost',
+        'PORT': '3306'
+    }
+}
+
+SECRET_KEY = 'django-insecure-4iy_31l!q4_-rbz-!^l#!wm=gwpz7=3*a+0)kuw-qf_r9skvc9'
