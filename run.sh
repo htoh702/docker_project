58f4549949e11136a2fc1d7fc3d4f9dd6c061629
htoh702 <htoh702@gmail.com>
2024-04-07 18:07:55 +0900

    add run.sh

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+MIGRATE_COMMAND="python manage.py migrate"
+RUNSERVER_COMMAND="python manage.py runserver 0.0.0.0:8000"
+
+RETRY_COUNT=5
+FAILED_COUNT=0
+
+while [ $FAILED_COUNT -lt $RETRY_COUNT ]
+do
+    $MIGRATE_COMMAND
+    if [ $? -eq 0 ]; then
+        echo "success"
+        $RUNSERVER_COMMAND
+        exit 0
+    else
+        echo "fail"
+        sleep 10
+        FAILED_COUNT=$((FAILED_COUNT+1))
+    fi
+done
+
+exit 1
